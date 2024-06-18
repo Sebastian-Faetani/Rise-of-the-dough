f2fb05fb677dd5d22ba1321379ce1ab1ede18b45
unknown <iniguezagustin2021@gmail.com>
2024-06-18 11:18:50 -0300

    main menu and play menu added

---
+++ scenes/UI/play_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	pass
+
+
+func _on_storybutton_pressed():
+	get_tree().change_scene_to_file("res://scenes/maps/lvl_1.tscn")
+
+
+func _on_sandboxbutton_pressed():
+	get_tree().change_scene_to_file("res://scenes/maps/sandbox.tscn")
+
+
+func _on_backbutton_pressed():
+	get_tree().change_scene_to_file("res://scenes/UI/main_menu.tscn")
