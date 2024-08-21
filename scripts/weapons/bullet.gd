019e2899504b5bfd0eb501a865815d8fe8695319
Sebastian Faetani <128256756+Sebastian-Faetani@users.noreply.github.com>
2024-08-21 17:49:57 -0300

    bullet functional

--- scripts/weapons/bullet.gd
+++ scripts/weapons/bullet.gd
@@ -1,9 +1,25 @@
 extends RigidBody3D
 
+@onready var gun_rays = $GunRays.get_children()
+@export var gun_damage = 10
+
 func _ready():
 	pass # Replace with function body.
-	
+
+func check_hit():
+	for ray in gun_rays:
+		if ray.is_colliding():
+			if ray.get_collider().is_in_group("enemies"):
+				ray.get_collider().DeathByMop(false)
+				ray.get_collider().DeathByWater(true)
+				ray.get_collider().enemyTakeDamage(gun_damage)
+			elif ray.get_collider().is_in_group("bossEnemy"):
+				ray.get_collider().DeathByMop(false)
+				ray.get_collider().DeathByWater(true)
+				ray.get_collider().enemyTakeDamage(gun_damage)
+			queue_free()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	check_hit()
 	
