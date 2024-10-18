c3af114c83be77ca0dc50456c7edcc382a0bba66
Sebastian Faetani <128256756+Sebastian-Faetani@users.noreply.github.com>
2024-10-18 00:54:59 -0300

    Decor y Masa Madre

--- scripts/enemies/boss_enemy.gd
+++ scripts/enemies/boss_enemy.gd
@@ -4,7 +4,7 @@ class_name Boss_Enemy
 #external variables
 @export var speed = 5
 @export var attack_range := 6.0
-@export var max_hp = 300
+@export var max_hp = 100
 @export var attack_damage := 30.0
 @export var range_shoot := 60.0
 @export var cooldown_time = 2
--- scripts/world/masa_3.gd
+++ scripts/world/masa_3.gd
@@ -2,6 +2,8 @@ extends StaticBody3D
 
 @onready var animation = $AnimatedSprite3D
 @onready var hide_text: Timer = $hide_text
+@onready var alive: Sprite3D = $Alive
+@onready var dead: Sprite3D = $Dead
 
 
 var player
@@ -9,9 +11,12 @@ var player
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
+	
 func SeRompeMasa3():
-	animation.play("dead")
+	alive.hide()
+	dead.show()
 	$CollisionShape3D.disabled = true
+	
 func MensajeMasa1():
 	player.lore_message.show()
 	player.lore_message.text = "Esto sería más facil con un poco de agua"
